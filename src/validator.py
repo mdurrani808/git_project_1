c7489dac0824551559a9e6cdb594a29773ad61c7
Andrew Wang <andrewxwg@gmail.com>
2026-03-23 10:25:37 -0400

    Add is_positive helper function

--- src/validator.py
+++ src/validator.py
@@ -25,3 +25,7 @@ def validate_positive(n):
         return num > 0
     except (ValueError, TypeError):
         return False
+
+def is_positive(n):
+    """Check if a number is positive."""
+    return n > 0
