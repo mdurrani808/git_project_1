fdd20f3b5e8452c0dc4350331cb059b121dc48d3
Patrick Hong <phong7@umd.edu>
2026-03-24 17:17:18 -0400

    Add is_positive helper function

--- src/validator.py
+++ src/validator.py
@@ -20,3 +20,8 @@ def validate_range(value, min_val=-1000, max_val=1000):
         return min_val <= num <= max_val
     except (ValueError, TypeError):
         return False
+
+def is_positive(n):
+    # Added this comment for checkpoint 4 step 2
+    """Check if a number is positive."""
+    return n > 0
